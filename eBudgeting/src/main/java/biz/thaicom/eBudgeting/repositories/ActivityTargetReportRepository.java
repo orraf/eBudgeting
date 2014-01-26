7cdf60f049aabd7b951b9589cb48a389f5b83651
dbuaklee <dbuaklee@hotmail.com>
2014-01-26 14:09:16 +0700

    fix query report for level 1 only

--- eBudgeting/src/main/java/biz/thaicom/eBudgeting/repositories/ActivityTargetReportRepository.java
+++ eBudgeting/src/main/java/biz/thaicom/eBudgeting/repositories/ActivityTargetReportRepository.java
@@ -77,6 +77,7 @@ public interface ActivityTargetReportRepository extends
 			+ "WHERE ( activity.regulator = ?1 "
 			+ "				OR activity.owner = ?1 ) "
 			+ "		AND activity.forObjective.fiscalYear = ?2 "
+			+ "		AND report.reportLevel = 1 "
 			+ "ORDER BY obj6.code asc, obj5.code asc, obj4.code asc, obj3.code asc, obj2.code asc, obj1.code asc, activity.code asc,"
 			+ "		target.id asc ")
 	public List<ActivityTargetReport> findAllByActivityOwnerOrRegulatorAndFiscalYear(
